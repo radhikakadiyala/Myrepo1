92aebab73a8e6053c60e8ab6a3262be538b8fa64
Radhika Kadiyala <65463298+radhikakadiyala@users.noreply.github.com>
2022-11-02 12:15:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,7 @@ def get_fruit_load_list():
      with my_cnx.cursor() as my_cur:
           my_cur.execute("select * from fruit_load_list")
           return my_cur.fetchall()
+
  #insert data    
  def insert_row_snowflake(new_fruit):
           with my_cnx.cursor() as my_cur:
